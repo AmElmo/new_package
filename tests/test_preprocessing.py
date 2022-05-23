3ec6c281521d7913ecd690d930669e522d8a7844
Julien Berthomier <berthomier.julien@gmail.com>
2022-05-23 15:49:44 +0100

    Done4

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,7 +1,7 @@
 from new_package.preprocessing import num_change, need_help
 
 def test_num_change():
-    assert type(num_change(10.5)) == int
+    assert type(num_change(10.5 + 11.2)) == int
 
 def test_need_help():
     assert need_help('test') == 'Good luck!'
